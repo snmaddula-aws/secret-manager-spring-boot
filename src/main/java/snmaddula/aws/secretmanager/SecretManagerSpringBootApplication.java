3ee7d04e3667f4b5295078e224e9c09b8a95bc12
snmaddula <snmaddula@outlook.com>
2018-12-21 17:14:55 +0530

    Initial Commit

---
+++ src/main/java/snmaddula/aws/secretmanager/SecretManagerSpringBootApplication.java
@@ -0,0 +1,14 @@
+package snmaddula.aws.secretmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretManagerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretManagerSpringBootApplication.class, args);
+	}
+
+}
+
